fe096a07802bdc4a5a2207e7c6427a0ae7e5cac3
theparam-asura <param930rajput@gmail.com>
2026-01-13 17:23:43 +0530

    Initial commit: Python data analytics practice

---
+++ task1.py
@@ -0,0 +1,9 @@
+import math
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt 
+#from datetime import datetime, timedelta
+#import numpy as np
+
+
+data =yf.downlaod("NG=F", start="2022-01-01")
