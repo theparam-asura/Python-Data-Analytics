869ca3cd10854d85e0eb558bd17236c51a185ca0
theparam-asura <param930rajput@gmail.com>
2026-01-13 19:11:16 +0530

    Task 2 completed

---
+++ task2.py
@@ -0,0 +1,20 @@
+import math
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt 
+#from datetime import datetime, timedelta
+import numpy as np
+
+
+data =yf.download("NG=F", start="2022-01-01")
+#print(data.head())
+
+#pecentage change calculation
+data['Simple Return'] =data['Close'].pct_change
+data['Log Retrun']=np.log(data['Close']/data['Close'].shift(1))
+monthly_volatility=data['Log Retrun'].groupby(data.index.month).std()
+print("Monthly Volatility:")
+print(monthly_volatility.sort_values(ascending=False))
+print("Simple Return:")
+print(data['Simple Return'])
+print(data['Log Retrun'])
